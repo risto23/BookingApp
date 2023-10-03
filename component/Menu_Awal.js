232965cb2518f1c56c5ba9721fb23c44da62aa01
Risto Kurniawan <risto9f@gmail.com>
2023-10-03 16:53:41 +0700

    udah mulai ada nih

--- component/Menu_Awal.js
+++ component/Menu_Awal.js
@@ -13,9 +13,12 @@ import {
     SafeAreaView,
     TextInput,
     Modal,
-    Linking
+    Linking,
+    FlatList
 } from 'react-native';
 import axios from 'axios';
+import { Card } from "@rneui/base";
+
 
 //autoresize gambar
 const dimensions = Dimensions.get('window');
@@ -24,6 +27,13 @@ const imageWidth = dimensions.width;
 
 export default main_program = ({ navigation }) => {
     //VARIABEL AWAL
+    let [json_hotel_room, setjson_hotel_room] = useState("");
+    let [json_hotel_detail, setjson_hotel_detail] = useState("");
+    let [json_hotel_param, setjson_hotel_param] = useState("");
+    let [json_hotel_price, setjson_hotel_price] = useState("");
+    let [hotel_expired, sethotel_expired] = useState("");
+    let [hotel_fasilitas, sethotel_fasilitas] = useState([]);
+
 
 
     // contoh axios pake get
@@ -57,18 +67,117 @@ export default main_program = ({ navigation }) => {
                 }
             })
             .then(async function (response) {
-               console.log(response.data)
+                // console.log(response.data.chosen_hotel.data.get_chosen_hotel.chosen_hotel_room)
+                setjson_hotel_room(response.data.chosen_hotel.data.get_chosen_hotel.chosen_hotel_room)
+                setjson_hotel_detail(response.data.chosen_hotel.data.get_chosen_hotel.chosen_hotel_detail)
+                setjson_hotel_param(response.data.chosen_hotel.data.get_chosen_hotel.chosen_hotel_params)
+                setjson_hotel_price(response.data.chosen_hotel.data.get_chosen_hotel.chosen_hotel_prices)
+                sethotel_expired(response.data.chosen_hotel.data.get_chosen_hotel.chosen_hotel_expired)
+                sethotel_fasilitas(response.data.chosen_hotel.data.get_chosen_hotel.chosen_hotel_detail.facilities)
+                // console.log(json_view_data.chosen_hotel_room.meal)
             })
-            .catch(error => {
-                console.log("ERROR !!!");
-                Alert.alert("ERROR", response.data.message);
-            });
     }
+
+    renderItem = ({ item }) => (
+        <View style={{ marginLeft: 5 }}>
+            
+                <ScrollView horizontal={true}>
+                    {/* <Text style={styles.tulisan1}> {item.kode_part} </Text> */}
+                    {/* <Text style={styles.tulisan2}>{item.url_gambar}</Text> */}
+
+                    <TouchableOpacity
+                        activeOpacity={0.3}
+                        style={{ marginRight: 5 }}
+                        onPress={() => {
+                            // navigation.navigate("PanZoom", {
+                            //     link: item.FILE_NAME,
+                            //     sumber: 'db'
+                            //     // sumber: 'db',
+                            //     // nama_foto : item.nama_foto,
+                            //     // row_id_foto : item.row_id
+                            // })
+                            console.log('ssss')
+                        }}
+                    >
+                        <Image
+                            source={{
+                                uri: item.thumbnail,
+                            }}
+                            style={{ marginTop: 20, marginBottom: 10, width: 100, height: 100 }}
+                        />
+
+
+                    </TouchableOpacity>
+
+                </ScrollView>
+           
+        </View>
+
+
+
+
+
+    )
+
+
+    show_fasilitas = () =>
+    {
+        let i = 0;
+        // console.log(a)
+        let panjang = hotel_fasilitas.length
+        console.log('pan :'+panjang)
+        for(i=0;i<panjang;i++)
+        {
+            return(
+                <View>
+                    <Text style={{ fontSize: 12, fontWeight: 'bold', marginTop: 5, color: '#000000' }}>{hotel_fasilitas[i]}</Text>
+                </View>
+            )
+        }
+    }
+
     
 
     return (
         <SafeAreaView style={{ flexDirection: 'column', flex: 1 }}>
             <Text>Halllooooo</Text>
+            <View>
+                <Card containerStyle={{ width: "100%", marginLeft: 0, backgroundColor: 'white' }} wrapperStyle={{}}>
+                <View>
+                        <FlatList
+                            horizontal={true}
+                            data={json_hotel_detail.images}
+                            // ItemSeparatorComponent = {FlatListItemSeparator}
+                            renderItem={renderItem}
+                        // ListEmptyComponent={EmptyListMessage}
+                        />
+                </View>
+                <View>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold', marginTop: 5, color: '#000000' }}>{json_hotel_detail.hotel_name}</Text>
+                </View>
+                <View>
+                        <Text style={{ fontSize: 12, fontWeight: 'bold', marginTop: 5, color: '#000000' }}>{json_hotel_detail.address}</Text>
+                </View>
+                <View style={{flexDirection:'row'}}>
+                    <ScrollView horizontal={true}>
+                        {hotel_fasilitas.map((item) =>
+                                <View style={{marginRight:5,borderWidth:1,borderRadius:10,backgroundColor:'grey'}}>
+                                    <View style={{padding:5}}>
+                                        <Text style={{ fontSize: 12, fontWeight: 'bold', marginTop: 5, color: '#000000' }}>{item}</Text>
+                                    </View>
+                                </View>
+                        )}
+                    </ScrollView>
+
+                </View>
+
+                <View>
+                        {/* <Text style={{ fontSize: 14, fontWeight: 'bold', marginTop: 5, color: '#000000' }}>{json_hotel_price.price_detail.currency} {json_hotel_price.price_detail.total}</Text> */}
+                </View>
+                    
+                    
+                </Card>
+            </View>
         </SafeAreaView>
     );
 }
